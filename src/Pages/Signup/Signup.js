b5f0abb934b191acd96f7b84d3dfc6ee2a8a1847
Lakshan Mihiranga <75910617+Lakshan098@users.noreply.github.com>
2022-07-26 00:11:32 +0530

    React dom initilized

---
+++ src/Pages/Signup/Signup.js
@@ -0,0 +1,14 @@
+import Navbar from '../../Components/Navbar/Admin/Navbar';
+import Footer from '../../Components/Footer/Footer';
+
+function Signup(){
+    return (
+        <div>
+            <Navbar/>
+            <h1>This is signup page</h1>
+          <Footer/>
+        </div>
+      );
+}
+
+export default Signup;

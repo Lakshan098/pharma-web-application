a9f5c6eb941f449c1842b353826c12d576d18748
sahan_dilshan <dsahan548@gmail.com>
2022-07-27 14:47:13 +0530

    Pharmacis login part ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import './Components/Footer/Footer';
+import './Components/Navbar/Pharmacist/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
       <div className="content">
+        <div>
+          
         <h1>PHARMA</h1>
+
+        </div>
+        </div>
       </div>
-    </div>
+    
   );
 }
 
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,67 @@
+//import Footer from '../../Components/Footer/Footer';
+//import Navbar from '../../Components/Navbar/Pharmacist/Navbar';
+import './Login.css';
+import loginlogo from '../../Assets/Brand/Loginlogo.jpeg';
+import logo from '../../Assets/Brand/Logo1.png';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { AiTwotoneMail } from 'react-icons/ai';
 
 
-function Login(){
-    return(
-        <div>
-            <Navbar />
-            <div>
-                <h1>This is login page. You first redirect here</h1>
+
+function Login() {
+    return (
+        <card>
+            <div className="main-div">
+
+                <div className="co-main">
+                    <img src={logo} className='logo-image' width={175} height={70} />
+                    <img src={loginlogo} className='login-image' width={500} height={300} />
+                </div>
+
+                <div className="login-text">
+                    <h3>Login</h3>
+                </div>
+
+
+                <Container fluid="md">
+                    <Row className="login-text">
+                        <Col>
+
+                            <input type="Email" className="Email" placeholder='Email' />
+                        </Col>
+
+                    </Row>
+
+                    <Row className="login-text">
+                        <Col>
+                            <input type="Password" className="password" placeholder='Password' />
+                        </Col>
+
+                    </Row>
+                </Container>
+
+                <div className="forgot-password">
+                    <h5>Forgot password?</h5>
+                </div>
+
+                <div>
+                    <button className="login-btn">Login</button>
+                </div>
+
+                <Container className="sin-up-btn" fluid="md">
+                    <Col><h5>Don't have an account ?Sin-UP</h5></Col>
+                    
+                    </Container>
+                    
+                
+
+
+
             </div>
-            <Footer />
-        </div>
+        </card>
     );
 }
 

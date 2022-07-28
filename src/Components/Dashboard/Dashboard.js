696b8bfd9e74a07e7246c06065a538e94befd03a
Navod Shehan Wimalaweera <navodshenz@gmail.com>
2022-07-28 08:59:06 +0530

    update

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -5,18 +5,22 @@ function Dashboard(){
         <div className="dashboard">
             <section class="home-section">
                 <div class="home-content">
+                    
                     <div class="member-stats">
-                        
                         <div class="one">
                             <p class="value">Customers</p>
                             <p class="name">2548 Customers</p>
                         </div>
+                    </div>
 
+                    <div class="member-stats">
                         <div class="two">
                             <p class="value">Pharmacies</p>
                             <p class="name">589 Pharmacies</p>
                         </div>
+                    </div>
 
+                    <div class="member-stats">
                         <div class="three">
                             <p class="value">Delivery Agents</p>
                             <p class="name">365 Delivery Agents</p>

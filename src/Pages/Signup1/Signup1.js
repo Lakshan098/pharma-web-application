c7b82961dc0344d671298ae2ff73286e5afc0450
Lakshan Mihiranga <75910617+Lakshan098@users.noreply.github.com>
2022-08-21 14:03:51 +0530

    Sigunp backend done

--- src/Pages/Signup1/Signup1.js
+++ src/Pages/Signup1/Signup1.js
@@ -4,8 +4,48 @@ import "../../index.css";
 import logo from "../../Assets/Brand/signup-photo.webp";
 import logo3 from "../../Assets/Brand/Logo1.png";
 import Button from "@mui/material/Button";
+import { useEffect, useRef, useState } from "react";
+import { Navigate, Link, useNavigate } from "react-router-dom";
+
+
+import Axios from '../../api/axios';
 
 function Signup1() {
+
+  const navigate = useNavigate();
+  const userRef = useRef();
+  const pharmacy = "pharmacy";
+
+  const [username, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [regNo, setRegNo] = useState('');
+  const [address, setAddress] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [accNo, setAccNo] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, [])
+
+  const HandleSignup = (e) => {
+    e.preventDefault()
+    const data = { username: username, email: email, password: password, telephone:telephone, regNo:regNo, accNo:accNo, user_type:pharmacy };
+    console.log(data);
+    Axios.post("http://localhost:3000/Signup", {
+      username: username, 
+      email: email, 
+      password: password, 
+      telephone:telephone, 
+      regNo:regNo, 
+      accNo:accNo, 
+      user_type:pharmacy
+    })
+    .then(() => {
+      navigate('/')
+    })
+};
+
   return (
     <div className="signup-container">
       <div className="signup-container2">
@@ -19,51 +59,86 @@ function Signup1() {
 
           <div className="signup-form-input">
             <form className="signup-input-form">
+              <label className="signup-form-label">Pharmacy Name</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                className="signup-username-password1"
+                ref={userRef}
+                autoComplete="off"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+                
+              />
+              <label className="signup-form-label">Pharmacy Registration Number</label>
+              <input
+                type="text"
+                name="regNo"
+                id="regNo"
+                className="signup-username-password1"
+                ref={userRef}
+                autoComplete="off"
+                value={regNo}
+                onChange={(e) => setRegNo(e.target.value)}
+              />
+              <label className="signup-form-label">Email</label>
               <input
                 type="text"
                 name="name"
                 className="signup-username-password1"
-                placeholder="Pharmacy Name"
+                ref={userRef}
+                autoComplete="off"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
+              <label className="signup-form-label">Pharmacy Address</label>
               <input
                 type="text"
-                name="name"
+                name="address"
+                id="address"
                 className="signup-username-password1"
-                placeholder="Pharmacy Registration Number"
+                ref={userRef}
+                autoComplete="off"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
               />
+              <label className="signup-form-label">Telephone</label>
               <input
                 type="text"
-                name="name"
+                name="telephone"
+                id="telephone"
                 className="signup-username-password1"
-                placeholder="Email"
+                ref={userRef}
+                autoComplete="off"
+                value={telephone}
+                onChange={(e) => setTelephone(e.target.value)}
               />
+              <label className="signup-form-label">Bank Account Number</label>
               <input
                 type="text"
-                name="name"
+                name="accNo"
+                id="accNo"
                 className="signup-username-password1"
-                placeholder="Pharmacy Address"
+                ref={userRef}
+                autoComplete="off"
+                value={accNo}
+                onChange={(e) => setAccNo(e.target.value)}
               />
-              <input
-                type="text"
-                name="name"
-                className="signup-username-password1"
-                placeholder="Pharmacy Account Number"
-              />
+              <label className="signup-form-label">Password</label>
               <input
                 type="password"
                 name="name"
                 className="signup-username-password1"
-                placeholder="Password"
+                ref={userRef}
+                autoComplete="off"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
-              <input
-                type="password"
-                name="name"
-                className="signup-username-password2"
-                placeholder="Confirm Passsword"
-              />
+
             </form>
             <div className="signup-btn">
-              <Button size="small" variant="contained" className="signup-btn-btn">
+              <Button size="small" variant="contained" className="signup-btn-btn" onClick={HandleSignup}>
                 SIGNUP
               </Button>
             </div>
